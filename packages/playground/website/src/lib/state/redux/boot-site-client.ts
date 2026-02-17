787b03c79748ea3b4d5aa6b35ab45014b86701da
Bero <berislav.grgicak@gmail.com>
2026-02-17 09:05:56 +0100

    Add WebMCP support to the Playground website

--- packages/playground/website/src/lib/state/redux/boot-site-client.ts
+++ packages/playground/website/src/lib/state/redux/boot-site-client.ts
@@ -36,6 +36,7 @@ import {
 } from '../../../github/git-auth-helpers';
 import { findFirewallErrorInCauseChain } from './error-utils';
 import { PHPMYADMIN_INSTALL_PATH } from '@wp-playground/tools';
+import { registerWebMCPTools } from '../../webmcp';
 
 export function bootSiteClient(
 	siteSlug: string,
@@ -151,6 +152,7 @@ export function bootSiteClient(
 				onClientConnected: (playgroundClient) => {
 					playground = (window as any)['playground'] =
 						playgroundClient;
+					registerWebMCPTools(playgroundClient);
 				},
 				// Log Blueprint events
 				onBlueprintValidated: logBlueprintEvents,
